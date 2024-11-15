8d728d0bcadc5ba1d7f2dd96640b7b1626dfd117
Зядик Дмитрий Сергеевич <dima@nvavia.ru>
2024-11-15 16:41:13 +0500

    init

---
+++ app/(dashboard)/layout.jsx
@@ -0,0 +1,8 @@
+export default function Layout({children}) {
+    return (
+        <section>
+            {children}
+        </section>
+    );
+}
+
---
+++ app/(dashboard)/tours/page.jsx
@@ -0,0 +1,5 @@
+export default function ToursPage() {
+    return (
+        <div>Tours Page</div>
+    );
+}
